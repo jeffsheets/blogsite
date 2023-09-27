76623ee20788cc59bbea443cebbbe6166d4cadad
Jeff Sheets <jeffsheets@gmail.com>
2023-09-26 20:05:48 -0500

    feat: first post setup

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,45 +1,35 @@
 module.exports = {
   url: process.env.URL || 'http://localhost:8080',
-  siteName: 'Eleventy Excellent',
+  siteName: 'Jeff Sheets - Uncommented Bytes',
   siteDescription:
-    'Eleventy starter based on the workflow suggested by buildexcellentwebsit.es.',
+    'Tech briefs and notes by Jeff Sheets',
   siteType: 'Person', // schema
   locale: 'en_EN',
   lang: 'en',
   skipContent: 'Skip to content',
-  author: 'Lene Saile', // i.e. Lene Saile - author's name. Must be set.
-  authorEmail: '', // i.e. hola@lenesaile.com - email of the author
-  authorWebsite: '', // i.e. https.://www.lenesaile.com - the personal site of the author
-  themeColor: '#DD4462', //  Manifest: defines the default theme color for the application
+  author: 'Jeff Sheets',
+  authorEmail: 'jeffsheets@gmail.com',
+  authorWebsite: 'https://www.sheetsj.com',
+  themeColor: '#1CAF7D', //  Manifest: defines the default theme color for the application
   themeBgColor: '#F3F3F3', // Manifest: defines a placeholder background color for the application page to display before its stylesheet is loaded
   meta_data: {
     opengraph_default: '/assets/images/opengraph-default.jpg', // fallback/default meta image
-    opengraph_default_alt:
-      'Visible content: Eleventy starter based on workflow for Cube CSS, Every Layout, Design Tokens and Tailwind for uitility, based on the concepts explained in buildexcellentwebsit.es ', // alt text for default meta image
-    twitterSite: '', // i.e. @site - twitter profile of the site
-    twitterCreator: '', // i.e. @author -  twitter profile of the site
-    mastodonProfile: '' // i.e. https://front-end.social/@lene - url to your mastodon instance/profile
+    opengraph_default_alt: 'sheetsj.com blog', // alt text for default meta image
+    twitterSite: '@sheetsj', // i.e. @site - twitter profile of the site
+    twitterCreator: '@sheetsj', // i.e. @author -  twitter profile of the site
+    mastodonProfile: 'https://hachyderm.io/@jeffsheets' // url to your mastodon instance/profile
   },
   blog: {
     // this is for the rss feed
-    name: 'My great Web Development Blog',
+    name: 'Jeff Sheets - Uncommented Bytes',
     description:
-      'Tell the word what you are writing about in your blog! It will show up on feed readers.'
+      'Tech briefs and notes by Jeff Sheets'
   },
   pagination: {
     itemsPerPage: 20
   },
   address: {
-    // edit all presets or leave empty. They are being used in the pages for privacy.md and imprint.md
-    firma: 'Organization name',
-    street: '123 Main St.',
-    city: 'Ciudad',
-    state: 'Estado',
-    zip: '12345',
-    mobileDisplay: '+34 1234567',
-    mobileCall: ' +341234567',
-    email: 'hola@yoursite.com',
-    cif: ''
+    email: 'jeffsheets@gmail.com'
   },
   menu: {
     closedText: 'Menu'
--- src/_data/navigation.js
+++ src/_data/navigation.js
@@ -1,27 +1,15 @@
 module.exports = {
   top: [
     {
-      text: 'About',
-      url: '/about/'
-    },
-    {
-      text: 'Get started',
-      url: '/get-started/'
-    },
-    {
-      text: 'Built with',
-      url: '/built-with/'
-    },
-    {
       text: 'Blog',
       url: '/blog/'
+    },
+    {
+      text: 'About',
+      url: '/about/'
     }
   ],
   bottom: [
-    {
-      text: 'Imprint',
-      url: '/imprint/'
-    },
     {
       text: 'Privacy',
       url: '/privacy/'
